5a50262c0150621af76c6897b37900e5594ef1f4
Leonid <mr.lenyaxxx@gmail.com>
2021-03-01 13:47:39 +0300

    save chats to json

--- src/main/java/ru/saburov/telegrambot/Bot.java
+++ src/main/java/ru/saburov/telegrambot/Bot.java
@@ -1,22 +1,40 @@
 package ru.saburov.telegrambot;
 
-import org.apache.log4j.Logger;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
-
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class Bot extends TelegramLongPollingBot {
 
     private final String BOT_NAME;
     private final String BOT_TOKEN;
+    private final String CHATS_PATH = "src/main/resources/chats.json";
+
+    private ObjectMapper objectMapper;
+    private File file;
+    private HashSet<String> chats;
 
     public Bot(String botName, String botToken) {
         super();
         this.BOT_NAME = botName;
         this.BOT_TOKEN = botToken;
+        this.objectMapper = new ObjectMapper();
+        this.file = new File(CHATS_PATH);
+        this.chats = readChatsFromJson();
     }
 
     @Override
@@ -31,16 +49,41 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        long chat_id = update.getMessage().getChatId();
+        long chatId = update.getMessage().getChatId();
 
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chat_id));
-        message.setText("Привет");
+        if (!chats.contains(String.valueOf(chatId))) {
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText("Привет, запомнил тебя.");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void writeChatInJson(String chatId) {
+        HashSet<String> chats = readChatsFromJson();
+        chats.add(chatId);
 
         try {
-            execute(message);
-        } catch (TelegramApiException e) {
+            objectMapper.writeValue(file, new ArrayList<>(chats));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public HashSet<String> readChatsFromJson() {
+        try {
+            List<String> li = objectMapper.readValue(file, new TypeReference<List<String>>(){});
+            return new HashSet<>(li);
+        } catch (IOException e) {
+            System.out.println("Json пустой");
+        }
+
+        return new HashSet<>();
+    }
+
 }
--- src/main/java/ru/saburov/telegrambot/BotApplication.java
+++ src/main/java/ru/saburov/telegrambot/BotApplication.java
@@ -1,9 +1,14 @@
 package ru.saburov.telegrambot;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 public class BotApplication {
@@ -11,11 +16,19 @@ public class BotApplication {
     private static final Map<String, String> getenv = System.getenv();
 
     public static void main(String[] args) {
-        try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+        Bot bot = new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN"));
+//        try {
+//            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+//            botsApi.registerBot(bot);
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//        SendMessage message = new SendMessage();
+////        message.setChatId();
+//        message.setText("Привет");
+//
+////        bot.execute();
+
     }
+
 }
